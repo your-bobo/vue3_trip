826202b91b201e92fcbf5b903fc8c184b0f4714f
bobo <1360601571@qq.com>
2023-01-03 01:00:27 +0800

    详情页轮播图完善

--- vue3_trip/src/views/detail/components/detail_swipe.vue
+++ vue3_trip/src/views/detail/components/detail_swipe.vue
@@ -4,8 +4,17 @@
       <van-swipe-item class="item" v-for="(item, index) in swipeData" :key="index">
         <img :src="item.url" :alt="item.title">
       </van-swipe-item>
-      <template #indicator="{ active, total }">
-        <div class="indicator">{{ active + 1 }}/{{ total }}</div>
+      <template #indicator="{ active }">
+        <div class="indicator">
+          <template v-for="(value, key, index) in swipeGroup" :key="index">
+            <div class="item" :class="{ active: swipeData[active]?.enumPictureCategory == key  }">
+              <span class="text">{{ getTitle(value[0].title) }}</span>
+              <span class="count" v-if="swipeData[active]?.enumPictureCategory == key">
+                {{ getCategoryIndex(swipeData[active]) }}/{{ value.length }}
+              </span>
+            </div>
+          </template>
+        </div>
       </template>
     </van-swipe>
   </div>
@@ -17,8 +26,29 @@ import { computed } from 'vue'
 
 const detailStore = useDetailStore()
 const swipeData = computed(() => {
-  return detailStore.detailInfos?.mainPart?.topModule?.housePicture?.housePics
+  return detailStore.detailInfos?.mainPart?.topModule?.housePicture?.housePics || []
 })
+
+const swipeGroup = computed(() => {
+  const group = {}
+  for (const item of swipeData.value) {
+    group[item.enumPictureCategory] = []
+  }
+  for (const item of swipeData.value) {
+    const valueArray = group[item.enumPictureCategory]
+    valueArray.push(item)
+  }
+  return group || []
+})
+
+const getTitle = (title) => {
+  return title.replace('：', '').replace('【', '').replace('】', '')
+}
+
+const getCategoryIndex = (item) => {
+  const valueArray = swipeGroup.value[item.enumPictureCategory]
+  return valueArray.findIndex(data => data === item) + 1
+}
 </script>
 
 <style lang="less" scoped>
